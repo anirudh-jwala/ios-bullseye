11d8c227c4bfd282cf909db87969c56f78bb2b84
Anirudh Jwala <anirudhsai2@gmail.com>
2022-07-29 20:19:56 +0530

    Adding first swiftui test cases for unit testing of the models

---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,27 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Anirudh on 29/07/22.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+    
+    var game: Game!
+    
+    override func setUpWithError() throws {
+        game = Game()
+    }
+    
+    override func tearDownWithError() throws {
+        game = nil
+    }
+    
+    func testExample() throws {
+        XCTAssertEqual(game.points(sliderValue: 50), 999)
+    }
+    
+}
