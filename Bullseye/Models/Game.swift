5edb5ba6fa9632fb9d1e83d0d66aa1cc974ac544
Anirudh Jwala <anirudhsai2@gmail.com>
2022-07-29 20:34:19 +0530

    Updated the point calculation logic with simpler code refactoring

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -13,14 +13,10 @@ struct Game {
     var round: Int = 1
     
     func points(sliderValue: Int) -> Int {
-        var difference: Int
+        var difference: Int = self.target - sliderValue
         
-        if sliderValue > self.target {
-            difference = sliderValue - self.target
-        } else if self.target > sliderValue {
-            difference = self.target - sliderValue
-        } else {
-            difference = 0
+        if difference < 0 {
+            difference *= -1
         }
         
         let awaredPoints: Int = 100 - difference
