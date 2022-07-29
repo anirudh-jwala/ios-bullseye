beb4233d05463a19673700d058de8f423cd21ceb
Anirudh Jwala <anirudhsai2@gmail.com>
2022-07-29 18:39:09 +0530

    Building base layout with swiftui views

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -2,20 +2,31 @@
 //  ContentView.swift
 //  Bullseye
 //
-//  Created by Apple on 29/07/22.
+//  Created by Anirudh on 29/07/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1...100)
+                Text("100")
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
